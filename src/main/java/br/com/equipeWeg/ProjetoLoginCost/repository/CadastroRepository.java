b9702ae2e57ef13511f6acbb38b0fb7f6ffc63c8
João Paulo Gomes Rodrigues <147559751+jpgomesr@users.noreply.github.com>
2024-11-26 21:22:19 -0300

    Revert "."

--- src/main/java/br/com/equipeWeg/ProjetoLoginCost/repository/CadastroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.equipeWeg.ProjetoLoginCost.repository;
-
-import br.com.equipeWeg.ProjetoLoginCost.model.CadastroUsuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CadastroRepository extends JpaRepository<CadastroUsuario, Long> {
-}

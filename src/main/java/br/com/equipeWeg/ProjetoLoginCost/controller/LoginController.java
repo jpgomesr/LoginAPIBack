563bd72819c5cbff62fe512be18877c04aabbb7d
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2024-11-27 14:15:36 -0300

    login api reworked

--- src/main/java/br/com/equipeWeg/ProjetoLoginCost/controller/LoginController.java
+++ src/main/java/br/com/equipeWeg/ProjetoLoginCost/controller/LoginController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/login")
 public class LoginController {
@@ -14,14 +17,17 @@ public class LoginController {
     private CadastroRepository repository;
 
     @PostMapping
-    public ResponseEntity<String> autenticarUsuario(@RequestBody CadastroUsuario usuario) {
+    public ResponseEntity<Map<String, String>> autenticarUsuario(@RequestBody CadastroUsuario usuario) {
+        Map<String, String> response = new HashMap<>();
 
         CadastroUsuario cadastroUsuario = repository.findByEmail(usuario.getEmail());
 
         if (cadastroUsuario != null && cadastroUsuario.getSenha().equals(usuario.getSenha())) {
-            return ResponseEntity.ok("Login Realizado com Sucesso!");
+            response.put("message", "Login Realizado com Sucesso!");
+            return ResponseEntity.ok(response); // Retorna JSON { "message": "Login Realizado com Sucesso!" }
+        } else {
+            response.put("error", "Credenciais incorretas!");
+            return ResponseEntity.status(401).body(response); // Retorna JSON { "error": "Credenciais incorretas!" }
         }
-
-        return ResponseEntity.status(401).body("Credenciais incorretas!");
     }
 }

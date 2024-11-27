d561923dc449badaff6e57d1da4824083251c504
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2024-11-26 21:20:56 -0300

    .

---
+++ src/test/java/br/com/equipeWeg/ProjetoLoginCost/ProjetoLoginCostApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.equipeWeg.ProjetoLoginCost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLoginCostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

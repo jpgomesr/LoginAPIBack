82c6770f7efdd6321a5a77774544e8fc2af8ea65
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2024-11-28 21:30:47 -0300

    CRUD produto

--- src/main/java/br/com/equipeWeg/ProjetoLoginCost/controller/ProdutoController.java
+++ src/main/java/br/com/equipeWeg/ProjetoLoginCost/controller/ProdutoController.java
@@ -4,10 +4,10 @@ package br.com.equipeWeg.ProjetoLoginCost.controller;
 import br.com.equipeWeg.ProjetoLoginCost.model.CadastroProduto;
 import br.com.equipeWeg.ProjetoLoginCost.repository.CadastroProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/produto")
@@ -21,6 +21,25 @@ public class ProdutoController {
         repository.save(produto);
         System.out.println(produto);
         return produto;
+    }
+
+    @GetMapping("/{sku}")
+    public CadastroProduto getProduto(@PathVariable String sku) {
+        return repository.findBySku(sku);
+    }
+
+    @GetMapping
+    public List<CadastroProduto> getAllProdutos() {
+        return repository.findAll();
+    }
+
+    @DeleteMapping("/{sku}")
+    public void deleteProduto(@PathVariable String sku) throws SQLException {
+        if (repository.findBySku(sku) != null) {
+            repository.delete(repository.findBySku(sku));
+        } else {
+            throw new SQLException();
+        }
     }
 
 }
--- src/main/java/br/com/equipeWeg/ProjetoLoginCost/repository/CadastroProdutoRepository.java
+++ src/main/java/br/com/equipeWeg/ProjetoLoginCost/repository/CadastroProdutoRepository.java
@@ -1,10 +1,11 @@
 package br.com.equipeWeg.ProjetoLoginCost.repository;
 
 import br.com.equipeWeg.ProjetoLoginCost.model.CadastroProduto;
+import br.com.equipeWeg.ProjetoLoginCost.model.CadastroUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CadastroProdutoRepository extends JpaRepository<CadastroProduto, Long> {
-
+    CadastroProduto findBySku(String sku);
 }
